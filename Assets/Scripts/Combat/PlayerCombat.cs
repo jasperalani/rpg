02d9dd21d08d04006d8e1b121a46e65aca03c687
Jasper Alani <jasperalani@gmail.com>
2023-10-20 01:32:44 +0100

    Adds auto attack range checking

--- Assets/Scripts/Combat/PlayerCombat.cs
+++ Assets/Scripts/Combat/PlayerCombat.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Player;
 using UnityEngine;
 
 namespace RPG.Combat
 {
     public class PlayerCombat : MonoBehaviour
     {
+
+        public float autoAttackRange = 10;
+        PlayerController playerControl;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            playerControl = GetComponent<PlayerController>();
+            autoAttackRange = 10;
         }
 
         // Update is called once per frame
@@ -18,9 +24,28 @@ namespace RPG.Combat
 
         }
 
-        public void selectEnemy(EnemyTarget target)
+        public void SelectEnemy(EnemyTarget target)
         {
             print("EnemySelected");
+        }
+
+        public void AutoAttack(EnemyTarget target)
+        {
+            print("Start auto attacking if we are in range");
+
+            bool isInRange = Vector3.Distance(this.transform.position, playerControl.selectedEnemy.transform.position) < autoAttackRange;
+
+            if (playerControl.selectedEnemy != null && !isInRange)
+            {
+                // Selected but not in range
+                print("Not in range to auto attack");
+                // TODO: Make animation for auto attack (combat stance)
+            } else if (playerControl.selectedEnemy != null && isInRange)
+            {
+                print("In range to auto attack");
+                // TODO: Make animation for auto attack (combat damage)
+
+            }
         }
 
     }
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 I am following [this](https://www.youtube.com/watch?v=Zc4SmKPdjSU&list=PLbbmTaHgSifx0hVwr-t80T95llXgZW_jB&index=3) tutorial to try and make a 3D RPG in Unity and also learn some things along the way!
 
-version 0.0.3
+version 0.0.4
